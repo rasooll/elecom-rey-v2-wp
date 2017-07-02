61017c923c825b67829629708220f1c99ead4967
rasoolsf <safari@rasool.id.ir>
2017-07-02 23:52:41 +0430

    ...

--- firstpage.php
+++ firstpage.php
@@ -14,6 +14,11 @@
 
 		</div>
 		<div class="col-lg-9 col-md-9 col-sm-12 col-xs-12"> <!-- Content area -->
+
+		<div class="alert alert-info alert-dismissible" role="alert">
+  			<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+  		<strong>Warning!</strong> Better check yourself, you're not looking too good.
+		</div>
 		
 		<?php while(have_posts()) : the_post();?> 
 		
