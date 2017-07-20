513a44d22481961be93009530bb88556ad9f12cd
rasoolsf <safari@rasool.id.ir>
2017-07-20 19:15:33 +0430

    add new pic to slideshow

--- top-sidebar.php
+++ top-sidebar.php
@@ -8,16 +8,33 @@
 			    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
 			    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
 			    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+			    <li data-target="#carousel-example-generic" data-slide-to="3"></li>
+			    <li data-target="#carousel-example-generic" data-slide-to="4"></li>
 			  </ol>
 
 			  <!-- Wrapper for slides -->
 			  <div class="carousel-inner" role="listbox">
-				    <div class="item active">
-				      <img src="<?php bloginfo('template_url');?>/slides/slide1.jpg" alt="...">
-				      <div class="carousel-caption">
-			        	نمای ورودی دانشگاه یادگار امام - شهرری
-			      	  </div>
-				    </div>
+
+			  		<div class="item active">
+			    		<img src="<?php bloginfo('template_url');?>/slides/slide4.jpg" alt="...">
+			    		<div class="carousel-caption">
+			        		برگزاری کلاس آموزش میکرو کنترلر AVR
+			      	  	</div>
+			    	</div>
+
+			    	<div class="item">
+			    		<img src="<?php bloginfo('template_url');?>/slides/slide6.jpg" alt="...">
+			    		<div class="carousel-caption">
+			        		بررسی مراحل ساخت ایده هاو تحلیل بخشهای مورد نیاز قطعه پیشنهادی
+			      	  	</div>
+			    	</div>
+
+			    	<div class="item">
+			    		<img src="<?php bloginfo('template_url');?>/slides/slide7.jpg" alt="...">
+			    		<div class="carousel-caption">
+			        		برگزاری کلاس آموزش متلب MATLAB
+			      	  	</div>
+			    	</div>
 
 					<div class="item">
 			    		<img src="<?php bloginfo('template_url');?>/slides/slide3.jpg" alt="...">
