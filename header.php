24bcd8855e5d345036a222ca4ba6be1da4bf8d4e
rasoolsf <safari@rasool.id.ir>
2017-07-25 13:17:24 +0430

    change some item in navbar

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
 <title><?php wp_title('|', true, 'right'); ?></title>
 
 <!-- Bootstrap -->
-<link href="<?php bloginfo('template_url');?>/css/bootstrap.min.css" rel="stylesheet">
+<link href="<?php bloginfo('template_url');?>/css/bootstrap.css" rel="stylesheet">
 <link href="<?php bloginfo('template_url');?>/css/bootstrap-rtl.css" rel="stylesheet">
 <!-- Awesome -->
 <link rel="stylesheet" href="<?php bloginfo('template_url');?>/css/font-awesome.min.css">
@@ -53,10 +53,15 @@ wp_head();
             <li><a href="<?php bloginfo('url');?>/category/news/%d8%a7%d8%ae%d8%a8%d8%a7%d8%b1-%da%af%d8%b1%d9%88%d9%87-%d8%a8%d8%b1%d9%82/">اخبار گروه برق</a></li>
           </ul>
     </li>
-    <li><a href="<?php bloginfo('url');?>/%d8%b9%d8%b6%d9%88%db%8c%d8%aa/"><i class="fa fa-user-plus icon-menu" aria-hidden="true"></i> عضویت </a></li>
+	<li><a href="<?php bloginfo('url');?>/#"><i class="fa fa-photo icon-menu" aria-hidden="true"></i> گالری تصاویر </a></li>
     <li><a href="<?php bloginfo('url');?>/telegram" title="آدرس کانال‌ها و گروه‌های درسی"><i class="fa fa-telegram icon-menu" aria-hidden="true"></i> تلگرام </a></li>
-	<li><a href="<?php bloginfo('url');?>/%d8%af%d8%b1%d8%a8%d8%a7%d8%b1%d9%87%e2%80%8c%db%8c-%d9%85%d8%a7/"><i class="fa fa-id-card icon-menu" aria-hidden="true"></i> درباره ما </a></li>
-	<li><a href="<?php bloginfo('url');?>/%d8%aa%d9%85%d8%a7%d8%b3-%d8%a8%d8%a7%d9%85%d8%a7/"><i class="fa fa-envelope icon-menu" aria-hidden="true"></i> تماس با ما</a></li>
+    <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="fa fa-university icon-menu" aria-hidden="true"></i> انجمن علمی <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="<?php bloginfo('url');?>/%d8%af%d8%b1%d8%a8%d8%a7%d8%b1%d9%87%e2%80%8c%db%8c-%d9%85%d8%a7/"><i class="fa fa-id-card icon-menu" aria-hidden="true"></i> درباره ما </a></li>
+            <li><a href="<?php bloginfo('url');?>/%d8%aa%d9%85%d8%a7%d8%b3-%d8%a8%d8%a7%d9%85%d8%a7/"><i class="fa fa-envelope icon-menu" aria-hidden="true"></i> تماس با ما</a></li>
+            <li><a href="<?php bloginfo('url');?>/%d8%b9%d8%b6%d9%88%db%8c%d8%aa/"><i class="fa fa-user-plus icon-menu" aria-hidden="true"></i> عضویت </a></li>
+          </ul>
+    </li>
 </ul>
 </div>
 
